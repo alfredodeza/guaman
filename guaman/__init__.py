4adadda09e377169c5a2778d840df0349caaef86
Alfredo Deza <alfredodeza@gmail.com>
2011-12-06 14:58:02 -0500

    better arguments and options

--- guaman/__init__.py
+++ guaman/__init__.py
@@ -9,5 +9,8 @@ class Commands(object):
             self.parse_args(argv)
 
     def parse_args(self, argv):
-        parser = argparse.ArgumentParser(description='A PostgreSQL query analyzer')
+        parser = argparse.ArgumentParser(add_help=True, description='A PostgreSQL query analyzer')
+        parser.add_argument('--import', action='store', dest='import_path', help='Path to log directory or csv file')
+        if not argv:
+            parser.print_help()
         parser.parse_args(argv)
